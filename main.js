bc89329453f9c7db8e26115c8d2ec60f843df746
lohith967 <72453060+lohith967@users.noreply.github.com>
2021-02-02 20:54:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -103,3 +103,39 @@ function my_keydown(e){
          console.log("c");
      }
 }
+function up(){
+    if (player_y>=0){
+        player_y=player_y-block_height;
+        console.log("block img height="+block_height);
+        console.log("when up arrow is pressed,X="+player_x+",Y="+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function down(){
+    if (player_y<=500){
+        player_y=player_y+block_height;
+        console.log("block img height="+block_height);
+        console.log("when down arrow is pressed,X="+player_x+",Y="+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function left(){
+    if (player_x>=0){
+        player_x=player_x-block_width;
+        console.log("block img width="+block_width);
+        console.log("when left arrow is pressed,X="+player_x+",Y="+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function right(){
+    if (player_x<=850){
+        player_x=player_x+block_width;
+        console.log("block img width="+block_width);
+        console.log("when right arrow is pressed,X="+player_x+",Y="+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
